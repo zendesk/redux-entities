36a41693362fc24c9427d29fe9a93440649730bb
Alex Robinson <arobinson@zendesk.com>
2018-03-23 11:17:46 +1100

    Fix and clean upcreate-by-id reducer test

--- src/utils/create-by-id-reducer.test.js
+++ src/utils/create-by-id-reducer.test.js
@@ -1,30 +1,32 @@
-import createEntityReducer from './create-by-id-reducer';
-
-jest.mock('./../reducers/by-id', () => jest.fn(() => jest.fn(state => ({ ...state, some: 'state' }))));
-const createById = require('./../reducers/by-id');
+jest.mock('./../reducers/by-id')
+const {default: createEntityReducer} = require('./create-by-id-reducer')
+const {default: createById} = require('./../reducers/by-id')
 
 describe('create-by-id-reducer', () => {
+
+  createById.mockImplementation((actionPayloadLocation) => state => ({...state, actionPayloadLocation}))
+
   it('throws an error if not passed in an object', () => {
-    expect(() => createEntityReducer(4)).toThrow();
+    expect(() => createEntityReducer(4)).toThrow()
   })
 
   it('returns a function', () => {
-    expect(typeof createEntityReducer('entities', {})).toBe('function');
+    expect(typeof createEntityReducer('entities', {})).toBe('function')
   })
 
   describe('reducer', () => {
-    const initialState = { cat: 'cat-state', dog: 'dog-state' };
-    const action = { type: 'SOME_ACTION' };
+    const initialState = {cat: 'cat-state', dog: 'dog-state'}
+    const action = {type: 'SOME_ACTION'}
     const reducers = {
       cat: jest.fn().mockReturnValue('new-cat-state'),
       dog: jest.fn().mockReturnValue('new-dog-state')
     }
-    let result;
+    let result
 
     beforeEach(() => {
-      const reducer = createEntityReducer('entities', reducers);
+      const reducer = createEntityReducer('entities', reducers)
 
-      result = reducer(initialState, action);
+      result = reducer(initialState, action)
     })
 
     it('calls each sub-state with its corresponding sub-reducer', () => {
@@ -34,7 +36,7 @@ describe('create-by-id-reducer', () => {
 
     it('returns the combined states', () => {
       expect(result).toEqual({
-        some: 'state',
+        actionPayloadLocation: 'entities',
         cat: 'new-cat-state',
         dog: 'new-dog-state'
       })
